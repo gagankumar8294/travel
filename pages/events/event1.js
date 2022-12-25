a75293738141a7f231d1c423d203d70d6191d08c
Gagan-Sky <gagankumar8294@gmail.com>
2022-12-26 00:34:00 +0530

    e c b

---
+++ pages/events/event1.js
@@ -0,0 +1,7 @@
+const page = () => {
+    return (
+        <h1>Event1 Page</h1>
+    )
+}
+
+export default page;
---
+++ pages/events/index.js
@@ -0,0 +1,7 @@
+const page = () => {
+    return (
+        <h1>Events page</h1>
+    )
+}
+
+export default page;
