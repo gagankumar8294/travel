b77f7436ef098c345bc541fbfb2310ec117e0dc8
Gagan-Sky <gagankumar8294@gmail.com>
2022-12-26 00:40:13 +0530

    third edit

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
-            Get started by editing&nbsp;
+            working on this&nbsp;
             <code className={styles.code}>pages/index.js</code>
           </p>
           <div>
